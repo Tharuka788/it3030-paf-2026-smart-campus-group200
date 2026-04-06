de9c7a799576bb6271dcc30d9e0cdad0ddf17bec
Rumen Kaluhendiwala <rumenhansaja4@gmail.com>
2026-04-06 20:23:47 +0530

    refactor User model to remove Lombok annotations and implement manual getters/setters

--- backend/src/main/java/com/smartcampus/model/User.java
+++ backend/src/main/java/com/smartcampus/model/User.java
@@ -1,17 +1,11 @@
 package com.smartcampus.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Set;
 
 @Document(collection = "users")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
     @Id
     private String id;
@@ -19,4 +13,55 @@ public class User {
     private String fullName;
     private String pictureUrl;
     private Set<String> roles; // ROLE_USER, ROLE_ADMIN, etc.
+
+    public User() {
+    }
+
+    public User(String id, String email, String fullName, String pictureUrl, Set<String> roles) {
+        this.id = id;
+        this.email = email;
+        this.fullName = fullName;
+        this.pictureUrl = pictureUrl;
+        this.roles = roles;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
 }
