a322793fe5d6de5d824be3c53ffbe7e5c042d047
Rumen Kaluhendiwala <rumenhansaja4@gmail.com>
2026-04-06 20:33:40 +0530

    feat: add firebase dependency to frontend and update maven test compile files in backend

--- backend/src/main/java/com/smartcampus/security/FirebaseTokenAuthenticationFilter.java
+++ backend/src/main/java/com/smartcampus/security/FirebaseTokenAuthenticationFilter.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashSet;
 
 @Component
