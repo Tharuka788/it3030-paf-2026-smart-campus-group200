61e1ee133927b940bec6b2184761e357a2ac59de
Danu20019 <it23206564@my.sliit.lk>
2026-04-21 20:39:43 +0530

    Update SecurityConfig.java

--- backend/src/main/java/com/smartcampus/config/SecurityConfig.java
+++ backend/src/main/java/com/smartcampus/config/SecurityConfig.java
@@ -71,6 +71,7 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+    
 
     @Bean
     public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
